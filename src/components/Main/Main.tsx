0799b76e41cbceb999c8645f6cbadd13f9757d8d
Nasuschnyi <91079054+Nasuschnyi@users.noreply.github.com>
2024-07-27 11:33:22 +0300

    add VideoSlider

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,13 @@
 import Title from '../UI/Title/Title';
 import style from './Main.module.scss';
 import MainPage from './MainPage/MainPage';
+import VideoSlider from './MainSwiper/VideoSlider';
 
 const Main = () => {
 	return (
 		<main className={style.main}>
 			<MainPage />
+			<VideoSlider />
 			<Title />
 		</main>
 	);
